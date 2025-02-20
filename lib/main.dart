8baa08a87b04e5db0104ef3957605aec24f13a22
JANNA_STEPHANIE_RAMIREZ_GRANADOS <a.22308051281287@cbtis128.edu.mx>
2025-02-20 19:35:02 +0000

    enviando

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,36 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MisWidgets());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisWidgets extends StatelessWidget {
+  const MisWidgets({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("RamirezG", style: TextStyle(color: Color(0xff15578e))),
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+        ),
+        body: const Column(
+          children: <Widget>[
+            Text('Janna Ramirez',
+                style: TextStyle(fontSize: 30, color: Colors.blue)),
+            Text('Mat: 22308051281287 Gpo 6J',
+                style: TextStyle(fontSize: 25, color: Colors.black38)),
+            Expanded(
+              child: FittedBox(
+                child: FlutterLogo(),
+              ),
+            ),
+          ],
         ),
       ),
     );
-  }
-}
+  } //build
+} // mis widgets
